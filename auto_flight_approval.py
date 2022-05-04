e72022cd075977f58b2a49eee641035111e3a23f
Günther Eder <geder@cs.sbg.ac.at>
2022-05-05 00:02:48 +0200

    works ok for accepting non-viol flights

--- auto_flight_approval.py
+++ auto_flight_approval.py
@@ -15,6 +15,7 @@ AIR_SPACE_CHECKER = "airspace-check.jar"
 DL_DIR = "flights/"
 CHROMEPATH = './chromedriver'
 FIREFOXPATH = '/opt/homebrew/bin/geckodriver'
+MANUAL_EVAL_DIR = './manual_eval/'
 
 
 # for years < current_year use 'https://www.xcontest.org/YEAR/world/en/flights/'
@@ -82,7 +83,7 @@ def init_webdriver(headless:bool=True):
         return webdriver.Chrome(service=service, options=options, service_args=['--verbose'])
 
 
-def scrap_approval_flight(args, driver, url):
+def scrap_approval_flight(args, driver, url, manual_eval_set:set):
     """ SCRAP and approve/disapprove flights """
     flights_approved, flights_disapproved, flights_error, pilot_not_approved = [], [], [], []
     flight_dict = dict()
@@ -119,6 +120,12 @@ def scrap_approval_flight(args, driver, url):
             flight_id = None
             continue
 
+        '''check if flight is already in our manual eval directory'''
+        if flight_id in manual_eval_set:
+            if args.verbose:
+                print(f'Skipping filght {flight_id}, its already in manual.')
+            continue
+
         a_tags = row.find_elements(by=By.TAG_NAME, value="a")
         link_igc = [l for l in a_tags if 'IGC' in l.text.upper()]
         link_approval = [l for l in a_tags if 'action=A' in l.get_attribute('href')]
@@ -146,41 +153,31 @@ def scrap_approval_flight(args, driver, url):
 
                 if os.path.isfile(igc_file_name):
                     verdict, detail, kml_file_name = validte_flight(igc_file_name)
-                    print(f"{flight_infos['pilot_name']} glider {flight_infos['glider']} {flight_infos['points']} p. and {flight_infos['km']} km")
-                    if verdict == 0 or verdict == 1:
+                    
+                    if verdict == 0:
                         flights_approved.append(flight_infos)
-                        '''click the approval link'''
-                        link_approval.click()
+                        approve_disapprove_flight(link=link_approval, driver=driver)
+                        if args.verbose:
+                            print(f"APPROVED {flight_infos['pilot_name']} glider {flight_infos['glider']} {flight_infos['points']} p. and {flight_infos['km']} km")
 
-                        '''first popup to be sure to approve'''
-                        try:
-                            WebDriverWait(driver, 5).until (ec.alert_is_present())
-                            alert = driver.switch_to.alert
-                            alert.accept()
-                        except TimeoutException:
-                            if args.verbose:
-                                print("1st alert does not exist")
-
-                        '''second popup for comments'''
-                        try:
-                            WebDriverWait(driver, 5).until (ec.alert_is_present())
-                            alert = driver.switch_to.alert
-                            alert.accept()
-                        except TimeoutException:
-                            if args.verbose:
-                                print("2nd alert does not exist")
-
-                        if verdict == 1:
-                            print(f'MAYBE-OK: look at {kml_file_name}, might be aigen/uberg LZ')
+                    elif verdict == 1:
+                        store_for_manual_eval(flight_infos=flight_infos, kml_file_name=kml_file_name)
+                        if args.verbose:
+                            print(f"Minor-Violation {flight_infos['pilot_name']} glider {flight_infos['glider']} {flight_infos['points']} p. and {flight_infos['km']} km")
+                    
                     else:
+                        store_for_manual_eval(flight_infos=flight_infos, kml_file_name=kml_file_name)
+                        flights_disapproved.append(flight_infos)
                         '''SANITY CHECKS'''
+                        if args.verbose:
+                            print(f"VIOLATION {flight_infos['pilot_name']} glider {flight_infos['glider']} {flight_infos['points']} p. and {flight_infos['km']} km")
                         if 'HG' in flight_infos['glider']:
-                            print(f"VIOLATION: maybe new HG LZ -> check manually  {flight_infos['link_flight_info']}")
+                            if args.verbose:
+                                print(f"maybe new HG LZ, glider: {flight_infos['glider']}")
                         else:
                             '''check if a single data point is faulty'''
-                            flights_disapproved.append(flight_infos)
-                            print('VIOLATION: click disapprove and send mail with explaination', link_disapproval.get_attribute('href'), detail, kml_file_name)
-                            mail = Mail(flight=flight_infos, kml_file_name=kml_file_name)
+                            #mail = Mail(flight=flight_infos, kml_file_name=kml_file_name)
+                            pass
                 else:
                     flights_error.append(flight_infos)
         else:
@@ -191,6 +188,41 @@ def scrap_approval_flight(args, driver, url):
             break
 
     return flights_approved, flights_disapproved, flights_error, pilot_not_approved
+
+
+def store_for_manual_eval(flight_infos:dict, kml_file_name:str):
+    date_of_flight = flight_infos['start_date']
+    if ' ' in date_of_flight:
+        date_of_flight = date_of_flight.split(' ')[0]
+    flight_id = flight_infos['flight_id']
+    pilot_name = flight_infos['pilot_name']
+    new_kml_file_name = f"{date_of_flight}_{pilot_name}_{flight_id}_{kml_file_name.replace('flights/', '')}"
+    os.rename(kml_file_name, os.path.join(MANUAL_EVAL_DIR, new_kml_file_name))
+
+    file_content = ""
+    for key in flight_infos:
+        file_content+= f"{key}: {flight_infos[key]}\n"
+
+    with open(os.path.join(MANUAL_EVAL_DIR,f"{date_of_flight}_{pilot_name}_{flight_id}_info.txt"), 'w') as f:
+        f.write(file_content)
+
+def approve_disapprove_flight(link, driver):
+    link.click()
+    '''first popup to be sure to approve'''
+    try:
+        WebDriverWait(driver, 5).until (ec.alert_is_present())
+        alert = driver.switch_to.alert
+        alert.accept()
+    except TimeoutException:
+        pass
+
+    '''second popup for comments'''
+    try:
+        WebDriverWait(driver, 5).until (ec.alert_is_present())
+        alert = driver.switch_to.alert
+        alert.accept()
+    except TimeoutException:
+        pass
 
 
 def login(driver):
@@ -229,6 +261,10 @@ def validte_flight(igc_file_name:str)->Tuple[int,str,str]:
         kml_file_for_mail = igc_file_name + '.kml'
         return (1,verdict,kml_file_for_mail)
 
+def get_manual_eval_set()->set:
+    ids = [f.split('_')[2] for f in os.listdir(MANUAL_EVAL_DIR) if f.endswith('.txt')]
+    return set(ids)
+
 
 def main():
     parser = argparse.ArgumentParser(description='XContest FlyForFun Automatic Flight Approval')
@@ -239,8 +275,10 @@ def main():
 
     driver = init_webdriver(headless=not args.non_headless)
 
+    manual_eval_set = get_manual_eval_set()
+
     # flight scrapping
-    app, dis, err, nonpilot = scrap_approval_flight(args=args, driver=driver,url=URL_APPROVAL)
+    app, dis, err, nonpilot = scrap_approval_flight(args=args, driver=driver,url=URL_APPROVAL, manual_eval_set=manual_eval_set)
 
     print(f"Approved {len(app)}, disapproved {len(dis)}, errors {len(err)}, pilot not approved {len(nonpilot)}")
 
